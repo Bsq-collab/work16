9008256b25121ee644ad9d226f9489ff78b72c3f
Bayan Berri <bberri@stuy.edu>
2017-12-13 16:03:28 -0500

    done with my part

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,24 +11,22 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  char message[256];
+  char message[HANDSHAKE_BUFFER_SIZE];
   // creates the well known pipe server waits for connection
-  mkfifo("server",0644);
-  printf("server pipe created\n");
-  int pipe= open("server",O_RDONLY);
-  *to_client=pipe;
+  mkfifo("wkp",0644);
+  printf("wkp pipe created\n");
+  int from_client= open("wkp",O_RDONLY);
   printf("to_client set to pipe\n");
   printf("Well known pipe waiting for client\n");
-  read(pipe,message,sizeof(message));
-  printf("message read\n");
+  read(from_client,message,sizeof(message));
+  printf("message read: %s\n",message);
   //remove pipe for security
-  //close(pipe);
   remove("server");
   printf("removed server byebye!!\n");
-  int m=open(message,O_WRONLY);
-  write(m,"writingback",100);
+  *to_client=open(message,O_WRONLY);
+  write(*to_client,ACK,sizeof(message));
   printf("Server wrote back\n");
-  return m;
+  return from_client;
 
 }
 
